13dc54d2603f6b068b51565c8e534c05448f144d
Walker Randolph Smith <walker@wearesoftwarecraftsmen.com>
2015-09-29 18:12:18 -0400

    Determine the largest palindrome that can be created by the prodcut of two three digit numbers

---
+++ src/__tests__/004/palindrome-test.js
@@ -0,0 +1,41 @@
+jest.dontMock('./../../app/problems/004/palindrome');
+
+describe('../src/app/problems/004/palindrome', () => {
+  let sut;
+  beforeEach(() => {
+      sut = require('./../../app/problems/004/palindrome');
+  });
+
+  describe('When determining which two, three digit numbers\' product creates the larget palindrome', () => {
+    let actual;
+    beforeEach(() => {
+       actual = sut.getLargestPalindrome();
+    });
+
+    it('should equal 906609', () => {
+     expect(actual).toBe(906609);
+    });
+  });
+
+  describe('When determining if a "906609" is a palindrome', () => {
+    let actual;
+    beforeEach(() => {
+       actual = sut.isPalindrome("906609");
+    });
+
+    it('should equal true', () => {
+     expect(actual).toBe(true);
+    });
+  });
+
+  describe('When determining if a "walker" is a palindrome', () => {
+    let actual;
+    beforeEach(() => {
+       actual = sut.isPalindrome("walker");
+    });
+
+    it('should equal false', () => {
+     expect(actual).toBe(false);
+    });
+  });
+});
