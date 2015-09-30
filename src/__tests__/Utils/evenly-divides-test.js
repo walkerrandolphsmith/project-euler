89c2f2348f99023a0df280cc7b1d53aabb9b8ef0
Walker Randolph Smith <walker@wearesoftwarecraftsmen.com>
2015-09-29 21:19:15 -0400

    Determine if positive integer n evenly divides positive integer k.

---
+++ src/__tests__/Utils/evenly-divides-test.js
@@ -0,0 +1,72 @@
+jest.dontMock('./../../app/problems/utils/Utils');
+
+describe('../src/app/problems/utils/Utils/evenlyDivides', () => {
+  let sut;
+  beforeEach(() => {
+      sut = require('./../../app/problems/utils/Utils').evenlyDivides;
+  });
+
+  describe('Given two positive non-zero values', () => {
+    const value1 = 10;
+    const value2 = 2;
+
+    describe('When determining if 10 is evenly divisble by 2', () => {
+      let actual;
+      beforeEach(() => {
+         actual = sut(value1, value2);
+      });
+
+      it('should be true', () => {
+       expect(actual).toBe(true);
+      });
+   });
+  });
+
+  describe('Given the same positive non-zero values', () => {
+    const value1 = 5;
+    const value2 = 5;
+
+    describe('When determining if 5 is evenly divisble by 5.', () => {
+      let actual;
+      beforeEach(() => {
+         actual = sut(value1, value2);
+      });
+
+      it('should be true', () => {
+       expect(actual).toBe(true);
+      });
+   });
+  });
+
+  describe('Given a positive even integer and a positive odd integer ', () => {
+    const value1 = 4;
+    const value2 = 5;
+
+    describe('When determining if 4 is evenly divisble by 5.', () => {
+      let actual;
+      beforeEach(() => {
+         actual = sut(value1, value2);
+      });
+
+      it('should be false', () => {
+       expect(actual).toBe(false);
+      });
+   });
+  });
+
+  describe('Given a positive integer and a greater, positive integer ', () => {
+    const value1 = 12;
+    const value2 = 45;
+
+    describe('When determining if 12 is evenly divisble by 45.', () => {
+      let actual;
+      beforeEach(() => {
+         actual = sut(value1, value2);
+      });
+
+      it('should be false', () => {
+       expect(actual).toBe(false);
+      });
+   });
+  });
+});
--- src/app/problems/utils/Utils.js
+++ src/app/problems/utils/Utils.js
@@ -10,7 +10,7 @@ export function GCD(a,b){
   return b;
 }
 
-function evenlyDivides(n,k){
+export function evenlyDivides(n,k){
   return (n % k === 0);
 }
 
