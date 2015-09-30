07f6d5212506201de3f7d14bececc06c49278892
Walker Randolph Smith <walker@wearesoftwarecraftsmen.com>
2015-09-29 21:20:13 -0400

    Determine the smallest positive integer that is divisble by both a and b such that a and b are positive integers.

---
+++ src/__tests__/Utils/lcm-test.js
@@ -0,0 +1,40 @@
+jest.dontMock('./../../app/problems/utils/Utils');
+
+describe('../src/app/problems/utils/Utils/LCM', () => {
+  let sut;
+  beforeEach(() => {
+      sut = require('./../../app/problems/utils/Utils').LCM;
+  });
+
+  describe('Given two non-zero values', () => {
+    const value1 = 4;
+    const value2 = 10;
+
+    describe('When determining the smallest positive integer divisble by 4 and 10.', () => {
+      let actual;
+      beforeEach(() => {
+         actual = sut(value1, value2);
+      });
+
+      it('should equal 20', () => {
+       expect(actual).toBe(20);
+      });
+   });
+  });
+
+  describe('Given the same non-zero values', () => {
+    const value1 = 5;
+    const value2 = 5;
+
+    describe('When determining the smallest positive integer divisble by 5 and 5.', () => {
+      let actual;
+      beforeEach(() => {
+         actual = sut(value1, value2);
+      });
+
+      it('should equal itself', () => {
+       expect(actual).toBe(5);
+      });
+   });
+  });
+});
