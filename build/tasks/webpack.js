d4bc863bc94e164a2a0a1d074b69f06030e75268
Walker Randolph Smith <walker@wearesoftwarecraftsmen.com>
2015-09-29 21:25:36 -0400

    Removed files that were intended to be used to display allow user interaction via a website.

--- build/tasks/webpack.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var gulp = require('gulp');
-var pack = require('./../lib/pack');
-
-gulp.task('webpack', [], function () {
-	return pack();
-});
--- src/app/index.jsx
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-import Styles from './styles/index.less';
-
-import React from 'react';
-import ReactDom from 'react-dom';
-import App from './components/App';
-
-React.render(<App/>, document.getElementById('app'));
