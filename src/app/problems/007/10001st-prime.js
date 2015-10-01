86f534098863f54225dd8d22c5259dc6daf6197c
Walker Randolph Smith <walker@wearesoftwarecraftsmen.com>
2015-10-01 12:21:43 -0400

    use ++ notation and seperate variable declarations.

--- src/app/problems/007/10001st-prime.js
+++ src/app/problems/007/10001st-prime.js
@@ -1,25 +1,26 @@
 export function getNthPrime(index){
-  var i, primes = [2, 3], n = 5;
+  var primes = [2, 3];
+  var n = 5;
 
-    function isPrime(n) {
-        var i = 1, p = primes[i],
-            limit = Math.ceil(Math.sqrt(n));
-        while (p <= limit) {
-            if (n % p === 0) {
-                return false;
-            }
-            i += 1;
-            p = primes[i];
-        }
-        return true;
-    }
+  function isPrime(n) {
+      var i = 1, p = primes[i],
+          limit = Math.ceil(Math.sqrt(n));
+      while (p <= limit) {
+          if (n % p === 0) {
+              return false;
+          }
+          i++;
+          p = primes[i];
+      }
+      return true;
+  }
 
-    for (i = 2; i <= index; i += 1) {
-        while (!isPrime(n)) {
-            n += 2;
-        }
-        primes.push(n);
-        n += 2;
-    }
-    return primes[index - 1];
+  for (var i = 2; i <= index; i++) {
+      while (!isPrime(n)) {
+          n += 2;
+      }
+      primes.push(n);
+      n += 2;
+  }
+  return primes[index - 1];
 }
