af573e6d01a1630871b78e723a1f3ac281570c45
Walker Randolph Smith <walker@wearesoftwarecraftsmen.com>
2015-10-01 13:50:15 -0400

    Add another test for product of pythagorean triplets whose sum is 1000

--- src/__tests__/009/pythagorean-triplet-test.js
+++ src/__tests__/009/pythagorean-triplet-test.js
@@ -20,4 +20,18 @@ describe('../src/app/problems/009/pythagorean-triplet', () => {
       });
     });
   });
+  describe('Given value 1000', () => {
+    var value = 1000;
+
+    describe('When determining the pythogrean triplet whose values\'s sum is equal to the given value', () => {
+      let actual;
+      beforeEach(() => {
+         actual = sut.getProductOfPythogreanTripletWhoseSumEquals(value);
+      });
+
+      it('should equal 31875000', () => {
+       expect(actual).toBe(31875000);
+      });
+    });
+  });
 });
